ac65061735da6b31cbab7d49d1dd5ee8d5a16be6
RHasanaj <hasanaj11@hotmail.com>
2018-02-15 23:45:26 +0100

    first commit

---
+++ Avr/Main/Main/main.c
@@ -0,0 +1,18 @@
+/*
+ * Main.c
+ *
+ * Created: 2018-02-15 23:27:37
+ * Author : Rilind
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
